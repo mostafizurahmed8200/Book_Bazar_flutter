7d6819a02a88d38d61bdbfbe1c4f1084221509cf
mostafizurahmed8200 <mostafizur.ahmed@jayamsolutions.com>
2024-04-21 19:22:00 +0530

    latest date 21.04.2024

--- lib/page/home/topweek_screen.dart
+++ lib/page/home/topweek_screen.dart
@@ -1,7 +1,10 @@
+import 'package:book_bazar/model/model_category.dart';
+import 'package:book_bazar/widget/card_widget_category.dart';
 import 'package:flutter/material.dart';
 
 import '../../constant/constant.dart';
 import '../../widget/appbar_widget.dart';
+import '../../widget/sliver_gridview_widget.dart';
 
 class HomeTopOfWeekPage extends StatefulWidget {
   const HomeTopOfWeekPage({super.key});
@@ -11,6 +14,20 @@ class HomeTopOfWeekPage extends StatefulWidget {
 }
 
 class _HomeTopOfWeekPageState extends State<HomeTopOfWeekPage> {
+  late List<ModelCategory> _topOfWeakList;
+  @override
+  void initState() {
+    super.initState();
+
+    _topOfWeakList = _fetchCategoryData();
+  }
+
+  List<ModelCategory> _fetchCategoryData() {
+    // Simulate fetching category data
+    return ModelCategory
+        .categoryList; // Replace this with your actual fetching logic
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -25,6 +42,25 @@ class _HomeTopOfWeekPageState extends State<HomeTopOfWeekPage> {
           },
         ),
       ),
+      body: _buildAllProductTabs(_topOfWeakList),
     );
   }
+}
+
+Widget _buildAllProductTabs(List<ModelCategory> categoryList) {
+  return GridView.builder(
+    shrinkWrap: true,
+    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCountAndFixedHeight(
+      crossAxisCount: 2,
+      crossAxisSpacing: 1,
+      mainAxisSpacing: 1,
+      height: 220.0, //48 dp of height
+    ),
+    itemCount: categoryList.length,
+    itemBuilder: (context, index) {
+      final allItem = categoryList[index];
+      return CardWidgetCategory(infoModel: allItem);
+    },
+    scrollDirection: Axis.vertical,
+  );
 }
