827b7c6a89dba1f46f96fb679bea8a956ae8f571
mostafizurahmed8200 <mostafizur.ahmed@jayamsolutions.com>
2024-04-21 10:37:22 +0530

    latest date 21.04.2024

--- lib/model/model_category.dart
+++ lib/model/model_category.dart
@@ -12,33 +12,53 @@ class ModelCategory {
 
   static List<ModelCategory> categoryList = [
     ModelCategory(
-      bookCover: Constant.cat1,
+      bookCover: Constant.slideImg2,
       bookName: "Memory",
       bookMoneyValue: "\$40.00",
     ),
     ModelCategory(
-      bookCover: Constant.cat2,
+      bookCover: Constant.slideImg3,
       bookName: "Soul",
       bookMoneyValue: "\$100.00",
     ),
     ModelCategory(
-      bookCover: Constant.cat3,
+      bookCover: Constant.slideImg4,
       bookName: "A Milion one",
       bookMoneyValue: "\$20.00",
     ),
     ModelCategory(
-      bookCover: Constant.cat4,
+      bookCover: Constant.slideImg5,
       bookName: "Alone",
+      bookMoneyValue: "\$70.00",
+    ),
+    ModelCategory(
+      bookCover: Constant.cat1,
+      bookName: "The Da Vinci Code",
+      bookMoneyValue: "\$40.00",
+    ),
+    ModelCategory(
+      bookCover: Constant.cat2,
+      bookName: "Carrie Fisher",
+      bookMoneyValue: "\$100.00",
+    ),
+    ModelCategory(
+      bookCover: Constant.cat3,
+      bookName: "The Good Sister",
+      bookMoneyValue: "\$20.00",
+    ),
+    ModelCategory(
+      bookCover: Constant.cat4,
+      bookName: "The Waiting",
       bookMoneyValue: "\$70.00",
     ),
     ModelCategory(
       bookCover: Constant.cat5,
-      bookName: "Alone",
+      bookName: "Where are your name",
       bookMoneyValue: "\$70.00",
     ),
     ModelCategory(
       bookCover: Constant.cat6,
-      bookName: "Alone",
+      bookName: "Bright Young Women",
       bookMoneyValue: "\$70.00",
     ),
   ];
--- lib/widget/card_widget_category.dart
+++ lib/widget/card_widget_category.dart
@@ -6,45 +6,40 @@ import '../model/model_category.dart';
 class CardWidgetCategory extends StatelessWidget {
   final ModelCategory infoModel;
 
-  const CardWidgetCategory({Key? key, required this.infoModel})
-      : super(key: key);
+  const CardWidgetCategory({super.key, required this.infoModel});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width:
-          double.infinity, // Make the card expand to fill the available width
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10.0),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
             borderRadius: BorderRadius.circular(5),
             child: Image.asset(
               infoModel.bookCover,
-              fit: BoxFit.cover,
-
+              fit: BoxFit.fill,
+              height: 150,
+              width: 200,
               // Adjust the fit to cover the whole area
             ),
           ),
-          SizedBox(height: 8), // Adding some space between the image and text
-          Column(
-            children: [
-              Text(
-                infoModel.bookName,
-                style:
-                    const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
-              ),
-              SizedBox(height: 2),
-              Text(
-                infoModel.bookMoneyValue,
-                style: const TextStyle(
-                  fontSize: 15,
-                  color: Constant.appColor,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ],
-          )
+          const SizedBox(
+              height: 8), // Adding some space between the image and text
+          Text(
+            infoModel.bookName,
+            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+          ),
+          const SizedBox(height: 2),
+          Text(
+            infoModel.bookMoneyValue,
+            style: const TextStyle(
+              fontSize: 15,
+              color: Constant.appColor,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
         ],
       ),
     );
